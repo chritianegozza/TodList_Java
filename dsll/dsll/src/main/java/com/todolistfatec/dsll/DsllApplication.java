61d5798354eb4fa53685664485da74a0e6abc2f3
christiane gozza <72118415+chritianegozza@users.noreply.github.com>
2023-12-02 12:53:38 -0300

    Projeto Final

---
+++ dsll/dsll/src/main/java/com/todolistfatec/dsll/DsllApplication.java
@@ -0,0 +1,14 @@
+package com.todolistfatec.dsll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsllApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DsllApplication.class, args);
+	}
+
+}
